d7f60191117360e8788174acfaac1b3c65aa26d1
David Sirit <davidsirit@yahoo.com>
2018-09-25 16:59:19 +0200

    resolved graph scale issues

--- timba-project-code/public/javascripts/addGraph.js
+++ timba-project-code/public/javascripts/addGraph.js
@@ -1,49 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ctx = document.getElementById("myChart").getContext("2d");
 
-document.addEventListener('DOMContentLoaded', () => {
-  
-  const ctx = document.getElementById('myChart').getContext('2d');
-  
-
-  let printChart = (()=> {
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
+  let chartData = {
+    labels: ["Vision", "Selfcontrol", "Courage", "Sharp"],
+    datasets: [{
+        label: "Player Stats A",
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        data: [1, 2, 3, 4]
+       } 
+      //{
+      //   label: "Player Stats B",
+      //   backgroundColor: "rgba(89, 222, 26, 0.2)",
+      //   borderColor: "rgba(89, 222, 26, 1)",
+      //   data: [3, 3, 3, 3]      }
+    ]
+  };
+  let chartOption = {
+    scale: {
+      ticks: {
+        beginAtZero: false,
+        min: 0,
+        max: 5,
+        stepZise: 1
       }
-  
+    },
+    legend: {
+      position: "bottom"
+    }
+  };
+
+  let printChart = (cData, cOption) => {
+    new Chart(ctx, {
+      type: "radar",
+      data: cData,
+      options: cOption
     });
+  };
 
-  });
-
-  printChart();
-})
+  printChart(chartData, chartOption);
+});
