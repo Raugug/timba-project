5bf64272e34ab2d0051c95298f4d9e87aeb987c2
David Sirit <davidsirit@yahoo.com>
2018-09-26 14:24:46 +0200

    remove id from URL

--- timba-project-code/routes/user.js
+++ timba-project-code/routes/user.js
@@ -10,7 +10,42 @@ const multer = require('multer');
 const upload = multer({ dest: './public/uploads/' });
 
 
-// Get Profile User
+// Get my Profile
+router.get('/myprofile', ensureLoggedIn(), (req, res, next) => {
+  User.findById(req.user._id)
+  .then(player => {
+    res.render('user/profile', {player});
+  }).catch(err => next(err))
+});
+//edit my profile
+router.get('/edituser', ensureLoggedIn(), (req, res, next) => {
+  User.findById(req.user._id)
+  .then(player => {
+    res.render('user/editProfile', {player});
+  }).catch(err => next(err))
+});
+// Get my Graph User 
+router.get('/graphuser', ensureLoggedIn(), (req, res, next) => {
+  User.findById(req.user._id).then(player => {
+    //console.log( `Grafico del player' ${player.username}`)
+    res.render('user/playerGraph', {
+      player, 
+      playerStr: JSON.stringify(player)
+    })
+  }).catch(err => next(err))
+});
+// Get Players List
+router.get('/list', ensureLoggedIn(), (req, res, nest) => {
+  User.find().then(players => {
+    console.log( `la lista tiene' ${players.length} jugadores`)
+    res.render('user/playerList', {players, list: 'listado de players'})
+  }).catch(e => next(e))
+});
+
+
+
+
+// Get other Profile User
 router.get('/:userId', ensureLoggedIn(), (req, res, next) => {
   User.findById(req.params.userId)
   .then(player => {
@@ -19,33 +54,10 @@ router.get('/:userId', ensureLoggedIn(), (req, res, next) => {
 });
 
 
-// Get Players List
-router.get('/list/:userId', ensureLoggedIn(), (req, res, nest) => {
-  User.find().then(players => {
-    console.log( `la lista tiene' ${players.length} jugadores`)
-    res.render('user/playerList', {players, list: 'listado de players'})
-  }).catch(e => next(e))
-});
 
-// Get Graph User 
-router.get('/graphuser/:userId', ensureLoggedIn(), (req, res, nest) => {
-  User.findById(req.params.userId).then(player => {
-    console.log( `Grafico del player' ${player.username}`)
-    res.render('user/playerGraph', {
-      player, 
-      playerStr: JSON.stringify(player)
-    })
-  }).catch(e => next(e))
-});
 
-//Edit User
+//Edit User /edituser/:userId
 
-router.get('/edituser/:userId', ensureLoggedIn(), (req, res, next) => {
-  User.findById(req.params.userId)
-  .then(player => {
-    res.render('user/editProfile', {player});
-  }).catch(e => next(e))
-});
 
 router.post('/edituser/:userId', ensureLoggedIn(), (req, res, next)=> {
   let {username, email} = req.body;
